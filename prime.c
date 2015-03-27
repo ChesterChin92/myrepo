2d2e96191c25adca5808d25ba088276e65d4b7e4
ChesterChin92 <chinschian@hotmail.com>
2015-03-27 16:41:51 +0800

    Initial Code for Color Changer Background
    - Full Test Completed

--- prime.c
+++ prime.c
@@ -1,1 +1,44 @@
-Testing
+__author__ = 'Chester Chin'
+
+
+import pygame
+from pygame.locals import *
+
+
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+white = (255,255,255)
+
+background_color = (50, 50, 255,)
+(width, height) = (800, 600)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Color Test')
+screen.fill(background_color)
+
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            background_color = white
+            print('Left Pressed')
+            screen.fill(red) #<--- Here
+            
+            pygame.display.update()
+
+        if event.key ==pygame.K_RIGHT:
+            background_color = blue
+            print('Right Pressed')
+            screen.fill(blue)
+            pygame.display.update()
+
+
+
